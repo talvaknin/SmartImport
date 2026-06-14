3f2de9876d4e201b0b7c02d3a2dee7e549065d8d
talvaknin <128133500+talvaknin@users.noreply.github.com>
2026-06-14 11:09:15 +0300

    Delete assets directory

--- assets/src-UW24ZMRV-CHsmRLzy.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-BQ4-hMlq.js";import"./index-DINQIS8l.js";var t=a;export{t as default};
--- assets/src-UW24ZMRV-Cf9_KYLe.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-zirJQyXs.js";import"./index-CDmMVxeV.js";var t=a;export{t as default};
